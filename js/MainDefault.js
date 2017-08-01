219d55e9b335c23c64453720b92c785661913112
Jian <314710634@qq.com>
2017-08-01 12:41:15 +0800

    finished Nginx and totop 2

--- js/MainDefault.js
+++ js/MainDefault.js
@@ -4,17 +4,28 @@
  * @date    2017-07-27 10:55:32
  * @version $Id$
  */
+function setBodyHeight() {
+    var mybodyHeight;
+    if (window.innerHeight) {
+        mybodyHeight = window.innerHeight;
+    } else {
+        mybodyHeight = document.body.clientHeight;
+    }
+    console.log(mybodyHeight);
+    var myheadHeight = document.getElementById('myhead').offsetHeight; //获取顶部div的高度
+    var needHeight = mybodyHeight - myheadHeight;
+    console.log(needHeight);
+    document.getElementById('mybody').style.height = needHeight + 'px';
+}
 
-function setBodyHeight(){
-	var mybodyHeight;
-        if (window.innerHeight) {
-            mybodyHeight = window.innerHeight;
-        } else {
-            mybodyHeight = document.body.clientHeight;
-        }
-         console.log(mybodyHeight);
-        var myheadHeight = document.getElementById('myhead').offsetHeight; //获取顶部div的高度
-        var needHeight = mybodyHeight - myheadHeight;
-         console.log(needHeight);
-        document.getElementById('mybody').style.height = needHeight + 'px';
+function setBodyWidth() {
+    var mybodyWidth;
+    if (window.innerWidth) {
+        mybodyWidth = window.innerWidth;
+    } else {
+        mybodyWidth = document.body.clientWidth;
+    }
+    console.log('mybodywidth:' + mybodyWidth);
+    var needWidth = mybodyWidth - 20; //padding 两侧边距各为10
+    document.getElementById('mybody').style.width = needWidth + 'px';
 }
